8d1226c4dfd26eb8a4617318ab7debb13c2aa65a
cryeprecision <muench.rico@gmail.com>
2023-09-16 22:58:14 +0200

    improve module name

--- src/openid/mod.rs
+++ src/openid/mod.rs
@@ -41,12 +41,12 @@
 
 pub(crate) mod constants;
 mod params;
+mod provider;
 mod response;
 mod validate;
-mod xml;
 mod xml_util;
 
 pub(crate) use params::*;
+pub(crate) use provider::*;
 pub(crate) use response::*;
 pub(crate) use validate::*;
-pub(crate) use xml::*;
