ef66b2e9215d005b175fa5f5a784f34cb3dc29a9
cryeprecision <muench.rico@gmail.com>
2023-09-16 20:39:29 +0200

    add module doc comment

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -1,3 +1,15 @@
+//! How this is intended to work
+//!
+//! # Manually Returned an Error
+//!
+//! TODO
+//!
+//! # Actix Returned an Error
+//!
+//! TODO
+
 mod app_error;
+mod error_handler;
 
 pub(crate) use app_error::*;
+pub(crate) use error_handler::*;
