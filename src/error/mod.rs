f4e405bc096e1cc9f14f4a6162c8f2838dcb8434
cryeprecision <muench.rico@gmail.com>
2023-09-17 23:47:31 +0200

    add todo to error handling documentation

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -2,6 +2,8 @@
 //!
 //! How error handling works
 //!
+//! TODO: Fix this
+//!
 //! 1) The endpoint hits the wall and returns `Err(...)`
 //! 2) `Responder::respond_to` for `Result<R,E>` is invoked
 //! 3) `HttpResponse::from_error` calls `error_response` on the `Error`
@@ -9,7 +11,7 @@
 //! 4) `AppError::error_response` is called if an `AppError` was the cause
 //! 5) An error handler is selected from the list of registered error handlers and invoked
 //!   1) If no error handler was found, the in (4) generated error response is returned
-//!   2) If the error handler returns `Err(...)`, ???
+//!   2) If the error handler returns `Err(...)`, that implements
 //! 6)
 //!
 //! # How it works
@@ -28,7 +30,7 @@
 //! ## Something Else
 //!
 //! ```text
-//! I don't know what happens before this
+//! Some Actix internal stuff
 //!   -> to_json_error (error handler)
 //!   -> Check the root casue -> Not an AppError -> Convert it
 //!   ->
