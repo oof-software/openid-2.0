e7b0e669f4a5f034e81dd8d8588feecfb76f26d8
cryeprecision <muench.rico@gmail.com>
2023-09-24 16:47:10 +0200

    add `cookies` route to view decrypted cookies

--- src/api/health.rs
+++ src/api/health.rs
@@ -1,20 +1,19 @@
+use std::ops::Deref;
+
 use actix_web::{web, HttpResponse};
 use reqwest::StatusCode;
 
 use crate::error::{AppResult, IntoAppError};
 
-#[actix_web::get("/live")]
 pub(crate) async fn health_live() -> AppResult<HttpResponse> {
     Ok(HttpResponse::Ok().body("LIVE"))
 }
 
-#[actix_web::get("/ready")]
 pub(crate) async fn health_ready() -> AppResult<HttpResponse> {
     Ok(HttpResponse::Ok().body("READY"))
 }
 
 /// Provide an example for an error response
-#[actix_web::get("/error")]
 pub(crate) async fn health_error() -> AppResult<HttpResponse> {
     Err(anyhow::anyhow!("stubbed toe 😖")
         .context("lost focus 😵")
@@ -23,8 +22,14 @@ pub(crate) async fn health_error() -> AppResult<HttpResponse> {
         .into_app_error_with_status(StatusCode::IM_A_TEAPOT))
 }
 
+/// Let the user view the encrypted cookies
+pub(crate) async fn health_cookies(session: actix_session::Session) -> AppResult<HttpResponse> {
+    Ok(HttpResponse::Ok().json(session.entries().deref()))
+}
+
 pub(crate) fn configure(cfg: &mut web::ServiceConfig) {
-    cfg.service(health_live)
-        .service(health_ready)
-        .service(health_error);
+    cfg.service(web::resource("/live").route(web::get().to(health_live)))
+        .service(web::resource("/ready").route(web::get().to(health_ready)))
+        .service(web::resource("/error").route(web::get().to(health_error)))
+        .service(web::resource("/cookies").route(web::get().to(health_cookies)));
 }
